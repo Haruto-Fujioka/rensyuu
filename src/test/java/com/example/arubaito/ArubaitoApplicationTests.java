c20bdfd735a9e49a36a3a5857b85ffd5f2bb9b68
f3156-haruto <haruto.f31056@gmail.com>
2025-12-01 12:04:41 +0900

    Initial commit

---
+++ src/test/java/com/example/arubaito/ArubaitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arubaito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArubaitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
